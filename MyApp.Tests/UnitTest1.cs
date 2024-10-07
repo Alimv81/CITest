b26fae9adcfd15b0a3bd64a9b3ffe4b98782e406
Ali Mohammadi Variani <ali.m.v.13811381@gmail.com>
2024-10-07 15:12:09 +0330

    added unit test project

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using CITest;
+using CITest.Models;
+using NUnit.Framework;
+
+namespace MyApp.Tests;
+
+public class Tests
+{
+    
+    private Calculator _calculator { get; set; } = null!;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Test]
+    public void Test_Add_ReturnsAddedNumbers()
+    {
+        double x = 5, y = 6;
+        double sum = _calculator.Add(x, y);
+        double trueSum = x + y;
+        
+        Assert.That(sum == trueSum);
+    }
+    
+    [Test]
+    public void Test_Multiply_ReturnsMultipliedNumbers()
+    {
+        double x = 5, y = 6;
+        double sum = _calculator.Multiply(x, y);
+        double trueSum = x * y;
+        
+        Assert.That(sum == trueSum);
+    }
+}
--- obj/Debug/net9.0/CITest.AssemblyInfo.cs
+++ obj/Debug/net9.0/CITest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CITest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7ce80ba82bc900f569ccb22f8519a7481bca83")]
 [assembly: System.Reflection.AssemblyProductAttribute("CITest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CITest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
